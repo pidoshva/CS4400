84a843cd6e98e4fc571e0d9c8c46f89a61275d8e
pidoshva <vpgeleus@gmail.com>
2024-09-27 17:47:28 -0600

    fixed to make sure it has the same names in both lists

--- sheetgenerator.py
+++ sheetgenerator.py
@@ -7,30 +7,32 @@ fake = Faker()
 # Function to generate random data for children's information
 def generate_child_data(num_entries=200):
     child_data = []
+    names = []
     for _ in range(num_entries):
         name = fake.first_name() + " " + fake.last_name()
+        names.append(name)
         address = fake.address()
         birth_date = fake.date_of_birth(minimum_age=0, maximum_age=18).strftime("%Y-%m-%d")
         ssn = fake.ssn()
         child_data.append([name, address, birth_date, ssn])
-    return pd.DataFrame(child_data, columns=["Child Name", "Address", "Birth Date", "SSN"])
+    return pd.DataFrame(child_data, columns=["Child Name", "Address", "Birth Date", "SSN"]), names
 
 # Function to generate random data for Medicaid information
-def generate_medicaid_data(num_entries=200):
+def generate_medicaid_data(names, num_entries=200):
     medicaid_data = []
-    for _ in range(num_entries):
+    for i in range(num_entries):
         medicaid_id = fake.unique.random_number(digits=9)
         mom_name = fake.first_name() + " " + fake.last_name()
         birth_date = fake.date_of_birth(minimum_age=0, maximum_age=18).strftime("%Y-%m-%d")
-        child_name = fake.first_name() + " " + fake.last_name()
+        child_name = names[i]  # Use the same child name from the first sheet
         medicaid_data.append([medicaid_id, mom_name, birth_date, child_name])
     return pd.DataFrame(medicaid_data, columns=["Medicaid ID", "Mom Name", "Child Birth Date", "Child Name"])
 
 # Main function to generate the files
 def generate_excel_files():
     # Generate two DataFrames with 200 entries each
-    child_data_df = generate_child_data(200)
-    medicaid_data_df = generate_medicaid_data(200)
+    child_data_df, child_names = generate_child_data(200)
+    medicaid_data_df = generate_medicaid_data(child_names, 200)
 
     # Save the data to Excel files
     child_data_filename = "children_data_200.xlsx"
