e5a910d36ce3c049a288c6665daf81978e08e871
19bduffy <brennon.j.duffy03@gmail.com>
2024-11-08 16:50:58 -0800

    Added crypto buttons

--- Source_Code/app.py
+++ Source_Code/app.py
@@ -66,6 +66,22 @@ class App:
         self.upload_existing_button = tk.Button(button_frame, text="Load Existing File", command=self.load_combined_data, width=30, height=2)
         self.upload_existing_button.pack(pady=10)
 
+        # Button to encrypt files
+        self.combine_button = tk.Button(button_frame, text="Encrypt File", command=self.encrypt_files, width=30, height=2)
+        self.combine_button.pack(pady=10)
+
+        # Button to decrypt files
+        self.combine_button = tk.Button(button_frame, text="Decrypt File", command=self.decrypt_file, width=30, height=2)
+        self.combine_button.pack(pady=10)
+
+        # Button to create encryption key
+        self.combine_button = tk.Button(button_frame, text="Generate Encryption Key", command=self.generate_encryption_key, width=30, height=2)
+        self.combine_button.pack(pady=10)
+
+        # Button to delete encryption key
+        self.combine_button = tk.Button(button_frame, text="Delete Encryption Key", command=self.delete_encryption_key, width=30, height=2)
+        self.combine_button.pack(pady=10)
+
         logging.info("UI widgets created.")
 
     def read_excel_file(self):
